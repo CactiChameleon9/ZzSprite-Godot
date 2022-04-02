ee69dcff6d336401655ef7c45762af8c721efa1a
CactiChameleon9 <51231053+CactiChameleon9@users.noreply.github.com>
2022-04-02 14:47:59 +0100

    Fix and cleanup ZzSpriteLibs.cs

--- ZzSpriteLibs.cs
+++ ZzSpriteLibs.cs
@@ -6,16 +6,14 @@ public class ZzSpriteLibs : Node
 	
 	public Int32 generate_seed(Int32 random_seed){
 		random_seed ^= random_seed << 13;
-//		GD.Print(random_seed);
-		random_seed ^= random_seed >> 17;
-//		GD.Print(random_seed);
+		random_seed ^= (Int32)((uint)random_seed >> 17);
 		random_seed ^= random_seed << 5;
 		return random_seed;
 		
 	}
 	
-	public Int32 add_int32(Int32 num1, Int32 num2, Int32 num3 = 0, Int32 num4 = 0){
-		return num1 + num2 + num3 + num4;
+	public Int32 add_int32(Int32 num1, Int32 num2, Int32 num3){
+		return num1 + num2 + num3;
 	}
 	
 }
